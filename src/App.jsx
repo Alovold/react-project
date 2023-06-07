c6b664d4ea2085b4ad45fb6341896d9c87962ec5
Austin Lovold <austin@lovolds.com>
2023-06-07 14:28:58 -0500

    Created react framework

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>This is the header, I promise</h1>
+        <p>another paragraph before the image!</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is so cool!
         </p>
+        <ul>
+          <li>a list item</li>
+          <li>a second list item</li>
+          <li>likely a third list item, time will tell...</li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
